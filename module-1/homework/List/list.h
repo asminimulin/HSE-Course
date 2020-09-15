93ad80343628097790450f317d35302d4e0c0d96
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-09-15 11:37:02 +0300

    Apply initial clang format on list.h

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -1,51 +1,41 @@
 #pragma once
 #include <cstddef>
 
-
 namespace task {
 
-
 class list {
+ public:
+  list();
+  list(size_t count, const int& value = int());
 
-public:
+  ~list();
+  list& operator=(const list& other);
 
-    list();
-    list(size_t count, const int& value = int());
+  int& front();
+  const int& front() const;
 
-    ~list();
-    list& operator=(const list& other);
+  int& back();
+  const int& back() const;
 
+  bool empty() const;
+  size_t size() const;
+  void clear();
 
-    int& front();
-    const int& front() const;
+  void push_back(const int& value);
+  void pop_back();
+  void push_front(const int& value);
+  void pop_front();
+  void resize(size_t count);
+  void swap(list& other);
 
-    int& back();
-    const int& back() const;
+  void remove(const int& value);
+  void unique();
+  void sort();
 
+  // Your code goes here?..
 
-    bool empty() const;
-    size_t size() const;
-    void clear();
-
-
-    void push_back(const int& value);
-    void pop_back();
-    void push_front(const int& value);
-    void pop_front();
-    void resize(size_t count);
-    void swap(list& other);
-
-
-    void remove(const int& value);
-    void unique();
-    void sort();
-
-    // Your code goes here?..
-
-private:
-
-    // Your code goes here...
-
+ private:
+  // Your code goes here...
 };
 
 }  // namespace task
