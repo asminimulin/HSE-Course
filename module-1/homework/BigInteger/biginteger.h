164d6869f3101658a94307717cfefa8c1bd0f051
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-05 14:08:54 +0300

    fix codestyle: add spacing between function declaration in header

--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -9,26 +9,33 @@
 
 namespace big_integer {
 
+// fixme: Think about removing redundant templated declarations
 class BigInteger {
  public:
   BigInteger();
+
   BigInteger(std::string_view string);  // NOLINT(google-explicit-constructor)
-  BigInteger(int value);                // NOLINT(google-explicit-constructor)
+
+  BigInteger(int value);  // NOLINT(google-explicit-constructor)
 
   BigInteger(const BigInteger& other);
+
   BigInteger(BigInteger&& other) noexcept;
 
   BigInteger& operator=(const BigInteger& other) = default;
+
   BigInteger& operator=(BigInteger&& other) noexcept = default;
 
   template <typename T>
   BigInteger operator+(const T& other) const {
     return *this + BigInteger(other);
   }
+
   BigInteger operator+(const BigInteger& other) const;
 
   BigInteger operator-(const BigInteger& other) const;
   template <typename T>
+
   BigInteger operator-(const T& other) const {
     return *this - BigInteger(other);
   }
@@ -40,6 +47,7 @@ class BigInteger {
   }
 
   BigInteger operator/(const BigInteger& other) const;
+
   template <typename T>
   BigInteger operator/(const T& other) const {
     return *this / BigInteger(other);
@@ -47,55 +55,70 @@ class BigInteger {
 
   BigInteger operator%(const BigInteger& other) const;
   template <typename T>
+
   BigInteger operator%(const T& other) const {
     return *this % BigInteger(other);
   }
 
   BigInteger& operator++();
+
   BigInteger operator++(int);  // NOLINT(cert-dcl21-cpp)
+
   BigInteger& operator--();
+
   BigInteger operator--(int);  // NOLINT(cert-dcl21-cpp)
 
   BigInteger operator-() const;
+
   BigInteger operator+() const;
 
   BigInteger& operator+=(const BigInteger& other);
+
   BigInteger& operator-=(const BigInteger& other);
+
   BigInteger& operator*=(const BigInteger& other);
+
   BigInteger& operator/=(const BigInteger& other);
+
   BigInteger& operator%=(const BigInteger& other);
 
   template <typename T>
   bool operator<(const T& other) const {
     return *this < BigInteger(other);
   }
+
   bool operator<(const BigInteger& other) const noexcept;
 
   template <typename T>
   bool operator<=(const T& other) const {
     return *this <= BigInteger(other);
   }
+
   bool operator<=(const BigInteger& other) const noexcept;
 
   bool operator>(const BigInteger& other) const noexcept;
+
   template <typename T>
   bool operator>(const T& other) const {
     return *this > BigInteger(other);
   }
 
   bool operator>=(const BigInteger& other) const noexcept;
+
   template <typename T>
   bool operator>=(const T& other) const {
     return *this >= BigInteger(other);
   }
 
   bool operator==(const BigInteger& other) const noexcept;
+
   template <typename T>
   bool operator==(const T& other) const {
     return *this == BigInteger(other);
   }
 
   bool operator!=(const BigInteger& other) const noexcept;
+
   template <typename T>
   bool operator!=(const T& other) const {
     return *this != BigInteger(other);
@@ -109,6 +132,7 @@ class BigInteger {
 
  private:
   static uint32_t GetHighPart(uint64_t value) noexcept;
+
   static uint32_t GetLowPart(uint64_t value) noexcept;
 
   enum CompareResult {
@@ -116,31 +140,47 @@ class BigInteger {
     EQUAL = 0,
     GREATER = 1,
   };
+
   static CompareResult CompareAbsoluteValues(
       const std::vector<uint32_t>& left, const std::vector<uint32_t>& right);
+
   static std::vector<uint32_t> SummarizeAbsoluteValues(
       const std::vector<uint32_t>& left, const std::vector<uint32_t>& right);
+
   static std::vector<uint32_t> SubtractAbsoluteValues(
       const std::vector<uint32_t>& left, const std::vector<uint32_t>& right);
+
   static std::vector<uint32_t> MultiplyAbsoluteValues(
       const std::vector<uint32_t>& left, const std::vector<uint32_t>& right);
+
   static std::vector<uint32_t> MultiplyAbsoluteValuesWithKaratsubaAlgo(
       const std::vector<uint32_t>& left, const std::vector<uint32_t>& right);
+
   static std::vector<uint32_t> MultiplyAbsoluteValuesNaive(
       const std::vector<uint32_t>& shorter,
       const std::vector<uint32_t>& longer);
+
   static std::pair<std::vector<uint32_t>, std::vector<uint32_t>>
   DivideAbsoluteValues(const std::vector<uint32_t>& left,
                        const std::vector<uint32_t>& right);
+
   static std::pair<std::vector<uint32_t>, uint32_t> DivideAbsoluteValueByLimb(
       const std::vector<uint32_t>& left, uint32_t limb);
+
   static void ShiftLimbsLeft(std::vector<uint32_t>& limbs, size_t count);
+
   static void ShiftLimbsRight(std::vector<uint32_t>& limbs, long count);
+
   static void ShiftBitsLeft(std::vector<uint32_t>& limbs, size_t count);
+
   static void ShiftBitsRight(std::vector<uint32_t>& limbs, long count);
+
   static constexpr uint32_t GetLeastBitsMask(size_t bits_count);
+
   static void Normalize(std::vector<uint32_t>& limbs) noexcept;
+
   static size_t GetMostSignificantBitPosition(uint32_t value) noexcept;
+
   static size_t GetMostSignificantBitPosition(
       const std::vector<uint32_t>& value) noexcept;
 
@@ -166,6 +206,7 @@ class BigInteger {
 };
 
 std::istream& operator>>(std::istream& in, BigInteger& value);
+
 std::ostream& operator<<(std::ostream& out, const BigInteger& value);
 
 }  // namespace big_integer
