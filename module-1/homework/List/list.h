13d409c44c87c68039f47c6d1e7ec1dc8d8c8a26
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-10 12:57:46 +0300

    codestyle: split lines on header file to be more convenient

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -5,11 +5,15 @@ namespace task {
 
 class list {
  public:
+
   list();
+
   list(size_t count, const int& value = int());
+
   list(const list& other);
 
   ~list();
+
   list& operator=(const list& other);
 
   int& front();
@@ -19,18 +23,25 @@ class list {
   const int& back() const;
 
   bool empty() const;
+
   size_t size() const;
+
   void clear();
 
   void push_back(const int& value);
   void pop_back();
+
   void push_front(const int& value);
   void pop_front();
+
   void resize(size_t count);
+
   void swap(list& other);
 
   void remove(const int& value);
+
   void unique();
+
   void sort();
 
 
