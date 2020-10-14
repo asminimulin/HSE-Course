fd1bb8aadae55b2e3a9302b970149cb4ed4b8ba0
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-14 21:27:04 +0300

    docs(hierarchy): Add docs for Point class

--- module-1/homework/Geometry/hierarchy/point.cpp
+++ module-1/homework/Geometry/hierarchy/point.cpp
@@ -8,6 +8,6 @@ bool Point::operator==(const Point &other) const {
     return this->x == other.x && this->y == other.y;
 }
 
-bool Point::operator!=(const Point &b) const {
-    return !(*this == b);
+bool Point::operator!=(const Point &other) const {
+    return !(*this == other);
 }
--- module-1/homework/Geometry/hierarchy/point.h
+++ module-1/homework/Geometry/hierarchy/point.h
@@ -1,23 +1,76 @@
 #pragma once
 
+/**
+ * @brief Point in Cartesian coordinate system
+ */
 struct Point {
+
+    /**
+     * @brief Constructs a point at the (0, 0)
+     */
     Point();
 
+    /**
+     * @brief Constructs a point at the (x, y)
+     *
+     * @param x X coordinate
+     * @param y Y coordinate
+     */
     Point(double x, double y);
 
+    /**
+     * @brief Constructs a point by copy semantics
+     *
+     * @param other The point needs to be copied
+     */
     Point(const Point &other) = default;
 
+    /**
+     * @brief Constructs Point by move semantics
+     *
+     * @param other The point needs to be moved
+     */
     Point(Point &&other) = default;
 
+    /**
+     * @brief Copies the provided point to this
+     *
+     * @param other The point needs to be copied
+     * @return The reference to the current Point
+     */
     Point &operator=(const Point &other) = default;
 
+    /**
+     * @brief Moved the provided point to this
+     *
+     * @param other The point needs to be moved
+     * @return The reference to the current Point
+     */
     Point &operator=(Point &&other) = default;
 
+    /**
+     * @brief Compares if this point is the same as other
+     *
+     * @param other The point needs to be compared to
+     * @return True if the points are equal, False otherwise
+     */
     bool operator==(const Point &other) const;
 
-    bool operator!=(const Point &b) const;
+    /**
+     * @brief Compares if this point is different from other
+     *
+     * @param other The point needs to be compared to
+     * @return True if the points are not equal, False otherwise
+     */
+    bool operator!=(const Point &other) const;
 
-
+    /**
+     * @brief The value of coordinate on OX axis
+     */
     double x;
+
+    /**
+     * @brief The value of coordinate on OY axis
+     */
     double y;
 };
