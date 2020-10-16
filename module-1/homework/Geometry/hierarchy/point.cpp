bc31de892203e736edf9c9dcd942aa61ccaad5fc
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-16 14:33:39 +0300

    feature(hierarchy.Point): use low precision double comparing from common.hpp

--- module-1/homework/Geometry/hierarchy/point.cpp
+++ module-1/homework/Geometry/hierarchy/point.cpp
@@ -1,13 +1,14 @@
 #include "point.h"
+#include "common.hpp"
 
 Point::Point() : x(0), y(0) {}
 
 Point::Point(double x, double y): x(x), y(y) {}
 
 bool Point::operator==(const Point &other) const {
-    return this->x == other.x && this->y == other.y;
+    return common::eq(this->x, other.x) && common::eq(this->y, other.y);
 }
 
 bool Point::operator!=(const Point &other) const {
-    return !(*this == other);
+  return common::ne(this->x, other.x) || common::ne(this->y, other.y);
 }
