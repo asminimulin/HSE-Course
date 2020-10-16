2c77071366397a44faafcf5d2b81346fe780c4eb
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-16 15:23:00 +0300

    feature(hierarchy.polygon): implement Polygon

---
+++ module-1/homework/Geometry/hierarchy/polygon.cpp
@@ -0,0 +1,274 @@
+#include "polygon.h"
+
+#include <algorithm>
+#include <exception>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+
+#include "common.hpp"
+
+Polygon::Polygon(std::vector<Point> vertices) : vertices_(std::move(vertices)) {
+  if (vertices_.size() < 3) {
+    throw std::logic_error(
+        "Cannot construct Polygon. At least 3 vertices required");
+  }
+
+  Normalize(vertices_);
+}
+
+Polygon::Polygon(const Polygon &other) : vertices_(other.vertices_) {}
+
+Polygon::Polygon(Polygon &&other) noexcept
+    : vertices_(std::move(other.vertices_)) {}
+
+Polygon &Polygon::operator=(const Polygon &other) {
+  vertices_ = other.vertices_;
+  return *this;
+}
+
+Polygon &Polygon::operator=(Polygon &&other) noexcept {
+  vertices_ = std::move(other.vertices_);
+  return *this;
+}
+
+size_t Polygon::verticesCount() const noexcept { return vertices_.size(); }
+
+std::vector<Point> Polygon::getVertices() const { return vertices_; }
+
+/**
+ * @details The polygon is convex when all points in walkthrough order
+ * are by the same side of polygon edge.
+ *
+ * I go through all edges, represented by the edge a->b
+ * and check if the next point c is by the positive side
+ * of vector a->b or by the negative one.
+ *
+ * Finally I check that there are no points by the different side
+ */
+bool Polygon::isConvex() const {
+  bool positive = false;
+  bool negative = false;
+
+  /**
+   * Trying to avoid if statements inside this for loop,
+   * because it has more chances to vectorize this operations
+   */
+  for (size_t pos = 0; pos < vertices_.size(); ++pos) {
+    auto next = (pos + 1) % vertices_.size();
+    auto next2 = (pos + 2) % vertices_.size();
+
+    auto a = vertices_[pos];
+    auto b = vertices_[next];
+    auto c = vertices_[next2];
+
+    auto vector_production = (Vector2(a, b) ^ Vector2(a, c));
+    positive |= common::gt(vector_production, 0);
+    negative |= common::lt(vector_production, 0);
+  }
+
+  return !(positive && negative);
+}
+
+double Polygon::perimeter() noexcept {
+  double result = 0;
+
+  for (auto i = 0; i < vertices_.size(); ++i) {
+    auto next = (i + 1) % vertices_.size();
+    result += common::dist(vertices_[i], vertices_[next]);
+  }
+
+  return result;
+}
+
+double Polygon::area() noexcept {
+  double result = 0;
+  auto basePoint = vertices_.front();
+
+  for (auto i = 1; i + 1 < vertices_.size(); ++i) {
+    auto vec1 = Vector2(basePoint, vertices_[i]);
+    auto vec2 = Vector2(basePoint, vertices_[i + 1]);
+    result += (vec1 ^ vec2);
+  }
+
+  return std::abs(result / 2);
+}
+
+bool Polygon::operator==(const Shape &other) const noexcept {
+  auto other_polygon = dynamic_cast<const Polygon *>(&other);
+  if (other_polygon == nullptr) {
+    // other is not even a polygon
+    return false;
+  }
+
+  return vertices_ == other_polygon->vertices_;
+}
+
+bool Polygon::isCongruentTo(const Shape &other) const noexcept {
+  auto other_polygon = dynamic_cast<const Polygon *>(&other);
+  if (other_polygon == nullptr) {
+    // other is not event a polygon
+    return false;
+  }
+
+  if (vertices_.size() != other_polygon->vertices_.size()) {
+    return false;
+  }
+
+  for (auto cur = 0; cur < vertices_.size(); ++cur) {
+    auto previous = (vertices_.size() + cur - 1) % vertices_.size();
+    auto next = (cur + 1) % vertices_.size();
+
+    auto this_A = vertices_[previous];
+    auto this_O = vertices_[cur];
+    auto this_B = vertices_[next];
+
+    auto other_A = other_polygon->vertices_[previous];
+    auto other_O = other_polygon->vertices_[cur];
+    auto other_B = other_polygon->vertices_[next];
+
+    auto this_length = common::dist(this_O, this_B);
+    auto other_length = common::dist(other_O, other_B);
+
+    if (common::ne(this_length, other_length)) {
+      return false;
+    }
+
+    auto this_angle =
+        Vector2::getAngle(Vector2(this_O, this_A), Vector2(this_O, this_B));
+    auto other_angle =
+        Vector2::getAngle(Vector2(other_O, other_A), Vector2(other_O, other_B));
+
+    if (common::ne(this_angle, other_angle)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool Polygon::isSimilarTo(const Shape &other) const noexcept {
+  auto other_polygon = dynamic_cast<const Polygon *>(&other);
+  if (other_polygon == nullptr) {
+    // other is not event a polygon
+    return false;
+  }
+
+  if (vertices_.size() != other_polygon->vertices_.size()) {
+    return false;
+  }
+
+  for (auto cur = 0; cur < vertices_.size(); ++cur) {
+    auto previous = (vertices_.size() + cur - 1) % vertices_.size();
+    auto next = (cur + 1) % vertices_.size();
+
+    auto this_A = vertices_[previous];
+    auto this_O = vertices_[cur];
+    auto this_B = vertices_[next];
+
+    auto other_A = other_polygon->vertices_[previous];
+    auto other_O = other_polygon->vertices_[cur];
+    auto other_B = other_polygon->vertices_[next];
+
+    auto this_angle =
+        Vector2::getAngle(Vector2(this_O, this_A), Vector2(this_O, this_B));
+    auto other_angle =
+        Vector2::getAngle(Vector2(other_O, other_A), Vector2(other_O, other_B));
+
+    if (common::ne(this_angle, other_angle)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool Polygon::containsPoint(Point point) const noexcept { return false; }
+
+void Polygon::rotate(Point center, double angle) noexcept {
+  angle = common::DegreesToRadians(angle);
+
+  for (auto &point : vertices_) {
+    auto x_centerized = point.x - center.x;
+    auto y_centerized = point.y - center.y;
+    auto new_x = center.x + (std::cos(angle) * x_centerized -
+        std::sin(angle) * y_centerized);
+    auto new_y = center.y + (std::sin(angle) * x_centerized +
+        std::cos(angle) * y_centerized);
+    point = {new_x, new_y};
+  }
+
+  Normalize(vertices_);
+}
+
+void Polygon::reflex(Point center) noexcept {
+  for (auto& point : vertices_) {
+    auto move = Vector2(point, center);
+    move *= 2;
+    point = {point.x + move.x, point.y + move.y};
+  }
+  Normalize(vertices_);
+}
+
+void Polygon::reflex(Line line) noexcept {
+  Point A, B;
+  std::tie(A, B) = line.Get2Points();
+  for (auto& point: vertices_) {
+
+    // Get Q
+    Vector2 pa(point, A);
+    Vector2 pb(point, B);
+    Vector2 ab(A, B);
+
+    auto PQ_length = (pa ^ pb) / ab.length();
+    auto AQ_length = std::sqrt(pa.length() - PQ_length);
+
+    auto ab_normalized = ab;
+    ab_normalized.normalize();
+
+    auto aq = ab_normalized;
+    aq *= AQ_length;
+
+    Point Q(A.x + aq.x, A.y + aq.y);
+
+    // Reflex point by Q
+    Vector2 move(point, Q);
+    move *= 2;
+
+    point = {point.x + move.x, point.y + move.y};
+  }
+  Normalize(vertices_);
+}
+
+void Polygon::scale(Point center, double coefficient) noexcept {
+  for (auto& point : vertices_) {
+    auto new_x = coefficient * point.x + (1 - coefficient) * center.x;
+    auto new_y = coefficient * point.y + (1 - coefficient) * center.y;
+    point = {new_x, new_y};
+  }
+  Normalize(vertices_);
+}
+
+void Polygon::Normalize(std::vector<Point> &vertices) {
+
+  auto lexicographical_less = [](const Point &a, const Point &b) {
+    return common::lt(a.x, b.x) ||
+        (common::eq(a.x, b.x) && common::lt(a.y, b.y));
+  };
+
+  auto min_vertex_it = std::min_element(vertices.begin(), vertices.end(),
+                                        lexicographical_less);
+
+  if (min_vertex_it != vertices.begin()) {
+    // Move subarray to the end of vector
+    std::rotate(vertices.begin(), min_vertex_it, vertices.end());
+
+    auto right = *++vertices.begin(); //< right by the vertices_.front()
+    auto left = vertices.back();      //< left by the vertices_.front()
+
+    if (common::lt(left.y, right.y) ||
+        (common::eq(left.y, right.y) && common::lt(left.x, right.x))) {
+      std::reverse(++vertices.begin(), vertices.end());
+    }
+  }
+}
---
+++ module-1/homework/Geometry/hierarchy/polygon.h
@@ -0,0 +1,182 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+#include "Vector2.hpp"
+#include "point.h"
+#include "shape.h"
+
+/**
+ * @brief Polygon on the Cartesian coordinate system
+ */
+class Polygon : public Shape {
+public:
+  /**
+   * @brief Constructs polygon with provided vertices
+   *
+   * It does not check that polygon has no self-intersections,
+   * the caller code should care of it
+   *
+   * @param vertices The vertices
+   */
+  explicit Polygon(std::vector<Point> vertices);
+
+  /**
+   * @brief Constructs polygon by moving other
+   *
+   * @param other The other polygon
+   *
+   * @throw std::bad_alloc() if failed to allocate memory for containing
+   * vertices
+   */
+  Polygon(const Polygon &other);
+
+  /**
+   * @brief Constructs polygon by moving other
+   *
+   * @param other The other polygon
+   */
+  Polygon(Polygon &&other) noexcept;
+
+  /**
+   * @brief Copies the other polygon to this
+   *
+   * @param other The other polygon
+   *
+   * @return Reference to this polygon
+   *
+   * @throw std::bad_alloc() if failed to allocate memory for containing
+   * vertices
+   */
+  Polygon &operator=(const Polygon &other);
+
+  /**
+   * @brief Moves the other polygon to this
+   *
+   * @param other The other polygon
+   *
+   * @return Reference to this polygon
+   */
+  Polygon &operator=(Polygon &&other) noexcept;
+
+  /**
+   * @brief The vertices count
+   *
+   * @return The vertices count
+   */
+  size_t verticesCount() const noexcept;
+
+  /**
+   * @brief Polygon's vertices
+   *
+   * @return Copied vertices
+   *
+   * @throw std::bad_alloc() if failed to allocate memory for vertices
+   */
+  std::vector<Point> getVertices() const;
+
+  /**
+   * @brief Check if the polygon is convex
+   *
+   * @return True if polygon is convex, false otherwise
+   */
+  bool isConvex() const;
+
+  /**
+   * @brief The perimeter of polygon
+   *
+   * @return The perimeter of polygon
+   */
+  double perimeter() noexcept override;
+
+  /**
+   * @brief The area of polygon
+   *
+   * @return The area of polygon
+   */
+  double area() noexcept override;
+
+  /**
+   * @brief Check if this Polygon is equal ot Shape other
+   *
+   * @param other Shape other
+   *
+   * @return True if this polygon is equal to Shape other, false otherwise
+   */
+  bool operator==(const Shape &other) const noexcept override;
+
+  /**
+   * @brief Check if this polygon is congruent to Shape other
+   *
+   * @param other Shape other
+   *
+   * @return True if this polygon is congruent to Shape other, false otherwise
+   */
+  bool isCongruentTo(const Shape &other) const noexcept override;
+
+  /**
+   * @brief Check if this polygon is similar to Shape other
+   *
+   * @param other Other shape
+   *
+   * @return True if this polygon is similar to Shape other, false otherwise
+   */
+  bool isSimilarTo(const Shape &other) const noexcept override;
+
+  /**
+   * @brief Check if point is inside this polygon
+   *
+   * @param point The point needs to be checked
+   *
+   * @return True if point is inside this polygon
+   */
+  bool containsPoint(Point point) const noexcept override;
+
+  /**
+   * @brief Rotate polygon relatively to Point center by angle(in degrees)
+   *
+   * @param center Center of rotation
+   *
+   * @param angle Angle the rotation applying by
+   */
+  void rotate(Point center, double angle) noexcept override;
+
+  /**
+   * @brief Reflex polygon relatively the center
+   *
+   * @param center Point by which reflection is applied
+   */
+  void reflex(Point center) noexcept override;
+
+  /**
+   * @brief Reflex polygon relatively the line
+   *
+   * @param line Line by which reflection is applied
+   */
+  void reflex(Line line) noexcept override;
+
+  /**
+   * @brief Scale polygon
+   *
+   * @param center Center of scale
+   *
+   * @param coefficient Coefficient of scale
+   */
+  void scale(Point center, double coefficient) noexcept override;
+
+  /**
+   * @brief Destructor
+   */
+  ~Polygon() noexcept override = default;
+
+protected:
+  static void Normalize(std::vector<Point> &vertices);
+
+  /**
+   * @brief Polygon's vertices.
+   *
+   * @details It is assumed that first Point is lexicographically minimal
+   */
+  std::vector<Point> vertices_;
+};
