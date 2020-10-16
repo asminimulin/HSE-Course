5593cb5cd3c700f6f3204d31f4e2deed11c3dd6a
Alexey Minimulin <a.s.minimulin@gmail.com>
2020-10-16 15:23:18 +0300

    feature(hierarchy.polygon): implement Line

--- module-1/homework/Geometry/hierarchy/line.cpp
+++ module-1/homework/Geometry/hierarchy/line.cpp
@@ -1,22 +1,20 @@
 #include "line.h"
-
+#include "common.hpp"
 
 bool Line::operator==(const Line &other) const {
-    /*
-     * a1x + b1y + c1 = 0
-     * a2x + b2y + c2 = 0
-     * a1 / a2 == b1 / b2 == c1 / c2
-     */
-    double a1 = this->a_, b1 = this->b_, c1 = this->c_;
-    double a2 = other.a_, b2 = other.b_, c2 = other.c_;
-    return a1 * b2 == a2 * b1 && a1 * c2 == a2 * c1 && b1 * c2 == b2 * c1;
+  /*
+   * a1x + b1y + c1 = 0
+   * a2x + b2y + c2 = 0
+   * a1 / a2 == b1 / b2 == c1 / c2
+   */
+  double a1 = this->a_, b1 = this->b_, c1 = this->c_;
+  double a2 = other.a_, b2 = other.b_, c2 = other.c_;
+  return a1 * b2 == a2 * b1 && a1 * c2 == a2 * c1 && b1 * c2 == b2 * c1;
 }
 
-bool Line::operator!=(const Line &other) const {
-    return !(*this == other);
-}
+bool Line::operator!=(const Line &other) const { return !(*this == other); }
 
-Line::Line(): a_(0), b_(0), c_(0) {}
+Line::Line() : a_(0), b_(0), c_(0) {}
 
 /*
  *    x - a.x       y - a.y
@@ -26,4 +24,24 @@ Line::Line(): a_(0), b_(0), c_(0) {}
  *  (x - a.x) * (a.y - b.y) == (y - a.y) * (a.x - b.x)
  *  x * (a.y - b.y) + y * (b.x - a.x) + a.x * b.y - a.y * b.x = 0
  */
-Line::Line(const Point &a, const Point &b): a_(a.y - b.y), b_(b.x - a.x), c_(a.x * b.y - a.y * b.x) {}
+Line::Line(const Point &a, const Point &b)
+    : a_(a.y - b.y), b_(b.x - a.x), c_(a.x * b.y - a.y * b.x) {}
+
+std::pair<Point, Point> Line::Get2Points() const noexcept {
+  if (common::ne(a_, 0)) {
+
+    double y = 10;
+    Point A((-c_ - b_ * y) / a_, y);
+    y = 20;
+    Point B((-c_ - b_ * y) / a_, y);
+    return {A, B};
+
+  } else {
+
+    double x = 10;
+    Point A(x, (-c_ - a_ * x) / b_);
+    x = 20;
+    Point B(x, (-c_ - a_ * x) / b_);
+    return {A, B};
+  }
+}
--- module-1/homework/Geometry/hierarchy/line.h
+++ module-1/homework/Geometry/hierarchy/line.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "point.h"
+#include <utility>
 
 class Line {
 public:
-    Line();
-    Line(const Point& a, const Point& b);
+  Line();
+  Line(const Point &a, const Point &b);
 
-    bool operator==(const Line& other) const;
-    bool operator!=(const Line& other) const;
+  bool operator==(const Line &other) const;
+  bool operator!=(const Line &other) const;
+
+  std::pair<Point, Point> Get2Points() const noexcept;
 
 private:
-    double a_, b_, c_;
+  double a_, b_, c_;
 };
